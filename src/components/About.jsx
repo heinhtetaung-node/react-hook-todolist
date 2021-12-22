4adf054cea395e5093c36a1510966d54f9aa7c6e
Hein Htet Aung <heinhtetaung.sglife@gmail.com>
2021-12-22 12:07:58 +0630

    react code test

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,12 +1,45 @@
 import Nav from './Nav'
+import React from 'react'
+
+const items = [ { text: 'Buy grocery', done: true },
+  { text: 'Play guitar', done: false },
+  { text: 'Romantic dinner', done: false }
+];
+
+class TodoList extends React.Component {
+    render() {
+      const { items, onListClick } = this.props;
+      return (<ul onClick={onListClick}>
+        {items.map((item, index) => 
+                   <TodoItem item={item} key={index} onClick={this.handleItemClick.bind(this, item)}/>)}
+      </ul>);
+    }
+    
+    handleItemClick(item, event) {
+      // Write your code here
+      if (item.done == true) {
+        event.stopPropagation()
+        return 
+      }
+      this.props.onItemClick(item, event)      
+    }
+}
+
+const TodoItem = (props) => <li onClick={props.onClick}>{props.item.text}</li>
 
 function About() {
     return (
         <div>
             <Nav />
-            <h1>About</h1>
+            <TodoList
+                items={items}
+                onListClick={(event) => console.log("List clicked!")}
+                onItemClick={(item, event) => { console.log(item, event) }}
+            />;
         </div>
     )
 }
+
+
 
 export default About
